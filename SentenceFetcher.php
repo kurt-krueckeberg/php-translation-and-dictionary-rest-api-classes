463794e4db75826b1ebcc9656368728d14cf3d2b
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-01-18 16:55:46 -0500

    u

--- SentenceFetcher.php
+++ SentenceFetcher.php
@@ -1,30 +1,36 @@
 <?php
+/*
+ *
+ GET Request for sentences for the word 'vernachlässigen', start with first sentnece and give me 10:
 
+ http://api.corpora.uni-leipzig.de/ws/sentences/deu_news_2012_1M/sentences/vernachl%C3%A4ssigen?offset=0&limit=10
+ *
+ */
 class SentenceFetcher {
 
    private $client = null;
 
-   //--private $auth_key;
-
    static $Service = "http://api.corpora.uni-leipzig.de/ws/sentences/";
+
+   private $uri;
 
    public function __construct(string $corpus)
    {
    
-      $uri = self::$Service . $corpus . '/sentences/'; 	   
+      $this->uri = self::$Service . $corpus . '/sentences/'; 	   
 
       $this->client = new Client();
 
       $this->header = "accept: application/json";
+
    }
 
-   public function get(string $word, $offset, $limit)
+   public function get(string $word, $offset = 0, $limit = 4)
    {
-      
-      // TODO: Figure out how to 'encode'  German umlauts for the query string
-      
       try {
-      
+
+	 $url = $this->uri . urlencode($word) . "?" . "offset=$offset&limit=$limit";   
+
          $response = $this->client->get($url, array("headers" => $header) );
       
          $result = $response->getBody()->getContents();
