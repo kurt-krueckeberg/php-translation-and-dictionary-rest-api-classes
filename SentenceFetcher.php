68001dd3dcab88ea02665c8f358802fef9bc82f8
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-01-19 11:57:47 -0500

    updates

--- SentenceFetcher.php
+++ SentenceFetcher.php
@@ -1,4 +1,8 @@
 <?php
+use \Guzzle\Http\Client as Client;
+
+include "vendor/autoload.php";
+
 /*
  *
  GET Request for sentences for the word 'vernachlässigen', start with first sentnece and give me 10:
@@ -8,30 +12,33 @@
  */
 class SentenceFetcher {
 
-   private $client = null;
-
-   static $Service = "http://api.corpora.uni-leipzig.de/ws/sentences/";
+   static $base_url = "http://api.corpora.uni-leipzig.de/ws/sentences/";
 
    private $uri;
+   private $base_uri;
 
-   public function __construct(string $corpus)
+   private $header;
+
+   public function __construct($corpus) 
    {
-   
-      $this->uri = self::$Service . $corpus . '/sentences/'; 	   
+      $this->uri = $corpus . '/sentences/'; 	   
 
-      $this->client = new Client();
+      $this->client = new Client($base_uri);
 
-      $this->header = "accept: application/json";
-
+      $this->header = "accept: application/json"; //<---???
    }
 
-   public function get(string $word, $offset = 0, $limit = 4)
+   public function get(string $word, $offset = 0, $limit = 7)
    {
+      /*
+       *  TODO: Guzzle Client requests can also be sent asynchronously. This can be done, too, using 'promise' objects like C++.
+       *
+       */  
+      $uri = $this->uri . urlencode($word);
+
       try {
 
-	 $url = $this->uri . urlencode($word) . "?" . "offset=$offset&limit=$limit";   
-
-         $response = $this->client->get($url, array("headers" => $header) );
+         $response = $client->request('GET', $uri, array('query' => array('offset' => 0, 'limit' => 10)) );
       
          $result = $response->getBody()->getContents();
       
@@ -41,7 +48,11 @@ class SentenceFetcher {
       
          $response = $this->StatusCodeHandling($e);
          return $response;
+
+      }  catch (\Exception $e) { 
+
+         return; // TODO: Should this be different?
       }
    }
+ 
 }
-
