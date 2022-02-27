07c66693a9528de96d55c4be77d46da1c77c1869
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-02-27 13:54:47 -0500

    u

---
+++ markd.php
@@ -0,0 +1,13 @@
+<?php
+
+
+ foreach ($file as $line) {
+	 if (strpos($line, "%table%") === 0)  /
+		 
+		 /* 
+		  * Append to .css file:
+	  * div table .table-col-percent-p1-p2-p3-etc {  }
+	   add_css(); 
+           add_div(); // <div class="table-x%-y%-z%-etc"> 
+             
+ }
