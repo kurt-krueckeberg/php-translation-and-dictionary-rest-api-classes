dd4e6b0a90e7b64a8b741695d8f7178c3442f7b1
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-02-27 15:57:15 -0500

    updates

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config['leipzip']['input_file'] = "words.txt";
+$config['leipzip']['sentence_cnt'] = 10;
+$config['deepl']['apikey'] = "7482c761-0429-6c34-766e-fddd88c247f9:fx";
+$config['deepl']['source_lang'] ='DE';
+$config['deepl']['target_lang'] = 'EN';
+$config['deepl']['output_format'] = 'html';
--- todo.md
+++ todo.md
@@ -1,10 +1,7 @@
 # TODO
 
-- SimpleXML code to read .xml config file, so we can get parameters
-
 - main.php implement:
 
-  - to use the above code
   - create SentenceFetcher
   - createe DeeplTranslatore
   - Read input words file and loop over it, fetching and translating sentences
