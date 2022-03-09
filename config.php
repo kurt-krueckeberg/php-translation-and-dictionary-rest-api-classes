25c8c0ed17a7309a79f41fb415783617821063d9
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-03-09 10:00:36 -0500

    updates

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$config['leipzig']['input_file'] = "words.txt";
+$config['leipzig']['input_file'] = "verbs-word-list.txt";
 $config['leipzig']['corpus'] = 'deu_news_2012_1M';
 $config['leipzig']['sentence_cnt'] = 10;
 $config['deepl']['apikey'] = "7482c761-0429-6c34-766e-fddd88c247f9:fx";
