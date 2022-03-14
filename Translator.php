57248f1c92c28457f60641b5f6898c764b635f8b
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-03-14 11:51:49 -0400

    updates

--- Translator.php
+++ Translator.php
@@ -11,28 +11,12 @@ class Translator implements Translate {
 
    private $api_impl; 
 
-   //public function __construct(\SimplXMLElement $el) // XML translation service section
-   public function __construct(string $fxml, string $service) // TODO: Add returns type of Translator. 
+   public function __construct(string $fxml, string $service) 
    {
       $this->api_iml = GuzzleTranslateAPIWarpper::create_implementor($xml_fname, $service);
    }
 
-   /*
-     Translate() returns an array of translated sentences, in which each element has two properties:
-
-      1. The detected_source_language - which, of course, we required to specified, so there is nothing to 'detect'.
-      2. "text" - the translated text in teh target lanauge.
-
-     Client code can iterate over this array like this:
-       $api_impllations = $tr->translate($input_sentence, $source_lang, $target_lang);
-            
-       foreach($api_impllations as $api_impllation) {
- 
-           do_something($de_sentence, $api_impllation->text);
-       } 
-   */
- 
-   public function translate(string $text, string $source_lang, string $target_lang) // TODO: Return what type of object 
+   public function translate(string $text, string $source_lang, string $target_lang) // TODO: Return what type of sentences object should be returned?
    {
        $api_impl->prepare_trans_request($text,  $source_lang,  $target_lang);
 
---
+++ xpath-explaned.txt
@@ -0,0 +1,8 @@
+# Understanding XPath Queries
+
+- W3School [XPath Tutorial](https://www.w3schools.com/xml/xpath_nodes.asp)   	
+- [XPath tutorial](https://www.softwaretestinghelp.com/xml-path-language-xpath-tutorial/)
+- Microsoft explnation of [XPath Queries](https://docs.microsoft.com/en-us/sql/relational-databases/sqlxml-annotated-xsd-schemas-xpath-queries/introduction-to-using-xpath-queries-sqlxml-4-0?view=sql-server-ver15)
+- Webscrapping tutorial uses [XPath](https://www.scrapingdog.com/blog/web-scraping-with-php)
+
+PHP's SimpleXMLElement has an xpath() function.
