f2fd12f61fd626d84d452521a75815b364c4724e
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-03-14 12:45:30 -0400

    updates

--- GuzzleTranslateAPIWrapper.php
+++ GuzzleTranslateAPIWrapper.php
@@ -3,27 +3,35 @@ declare(strict_types=1);
 
 // Generic rest translation API wrapper for guzzle/http users.
 abstract class GuzzleTranslateAPIWarpper {
-    
+
+   /*
+      See xpath-explained.md   
+   */
+    static $xpath_part1 = "/sentence_generation/translation_services/service/abbrev[normalize-space() = '";
+
+    static $xpath_part2 = "']/.."; 
+
     static public function create_implementor(string $xml_fname, string $service)
     {
-       // todo: Add logic to get the SimpleXMLElement pointer (to specific translation sectionn) using an XPath query.
+       $query = self::$xpath_part1 . $service . self::$xpath_part2;
+
        $simpl = simplexml_load_file($xml_fname);
 
-       // create $query based on $service
-       $simpl_ele = $simpl->xpath($query)  
+       $trans_service = $simpl->xpath($query);
  
        // Create the Derived Translator classes that use Guzzle
-       switch($simpl_ele->name) { 
+       switch($trans_service) { 
 
-         case 'I':
-           $trans = new IbmXXXTranslator($el);
+         case 'i':
+           $trans = new IbmTranslator($trans_service);
            break;
 
-        case 'M':
-           $trans = new MSTranlator($el);
+        case 'm':
+           $trans = new MSTranlator($trans_service);
            break;        case 'M':
 
-           $trans = new DeeplTranslate($el);
+        case 'd':
+           $trans = new DeeplTranslate($trans_service);
            break;
        }
        return $trans;
--- xpath-explaned.md
+++ xpath-explaned.md
@@ -2,7 +2,9 @@
 
 - W3School [XPath Tutorial](https://www.w3schools.com/xml/xpath_nodes.asp)   	
 - [XPath tutorial](https://www.softwaretestinghelp.com/xml-path-language-xpath-tutorial/)
-- Microsoft explnation of [XPath Queries](https://docs.microsoft.com/en-us/sql/relational-databases/sqlxml-annotated-xsd-schemas-xpath-queries/introduction-to-using-xpath-queries-sqlxml-4-0?view=sql-server-ver15)
-- Webscrapping tutorial uses [XPath](https://www.scrapingdog.com/blog/web-scraping-with-php)
+- [XPath Tutorial](https://www.educba.com/xml-features/?source=leftnav)
+- [Getting Started with XPath](https://riptutorial.com/xpath) is Exellent!
+
+  - [Find all elements with certain text](https://riptutorial.com/xpath/example/6209/find-all-elements-with-certain-text)
 
 PHP's SimpleXMLElement has an xpath() function.
