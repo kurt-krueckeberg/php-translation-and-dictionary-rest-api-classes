dea06475034ae60b8ea9f7d0470416a9ed59bb59
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-03-23 10:33:49 -0400

    u

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config['leipzig']['input_file'] = "verbs-word-list.txt";
-$config['leipzig']['corpus'] = 'deu_news_2012_1M';
-$config['leipzig']['sentence_cnt'] = 10;
-$config['deepl']['apikey'] = "7482c761-0429-6c34-766e-fddd88c247f9:fx";
-$config['deepl']['source_lang'] ='DE';
-$config['deepl']['target_lang'] = 'EN';
