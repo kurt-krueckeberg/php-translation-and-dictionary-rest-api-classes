55e82f4adce53a4ec15557e3d0ef0e876f50f9ea
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-13 18:26:09 -0400

    updates

--- FileReader.php
+++ FileReader.php
@@ -1,12 +1,11 @@
 <?php
 
-class FileReader extends \SplFileObject {
+class File extends \SplFileObject {
 
   public function __construct(string $fname)
   {
      parent::__construct($fname, "r");
 
      $this->setFlags(\SplFileObject::READ_AHEAD | \SplFileObject::SKIP_EMPTY | \SplFileObject::DROP_NEW_LINE);
-     
   }
 }
