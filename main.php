a9c5ae3829df9833f8b15cbb7a982fa809dff1ac
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-13 21:20:14 -0400

    updates

--- main.php
+++ main.php
@@ -8,6 +8,15 @@ include "SentenceFetcher.php";
 include "WebPageCreator.php";
 include "FileReader.php";
 
+function check_args(array $argv)
+{
+  if (strlen($argv[0]) !== 1 || $argv[0] !== 'd' ||  $argv[0] !== 'm' ||  $argv[0] !== 'i')
+        throw new \Exception("First argument must be 'd', 'm' or 'i'");
+
+  if (substr($argv[1], strpos($argv[1], ".")) !== "xml")
+        throw new \Exception("2nd argument must be config.xml file");
+
+}
   if ($argc < 2) {
   
       echo "Enter name of translation service (i for IBM, m for Microsoft or d for DEEPL), and the name of the output HTML file (omit .html)\n";
