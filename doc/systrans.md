8d71c13d59dd4c17e75fcbecb35a5f2fc5114ce0
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-20 18:46:56 -0400

    updates

---
+++ doc/systrans.md
@@ -0,0 +1,15 @@
+# Systran
+
+Website is  [Systran.net](https://www.systran.net/en/translate/)
+
+- The off a 14 day [free trial](https://www.systran.net/en/free-trial/) that does not require a credit card.
+   
+ - They support tons of languages 
+
+ - Pricing: Lite Plan cost: $5.50/month
+
+## REST APIs
+
+- [Systrans Translation API]https://docs.systran.net/translateAPI/translation)
+
+- [Dictionary lookup]https://docs.systran.net/translateAPI/dictionary)
---
+++ src/SystranTranslator.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types=1);
+namespace LanguageTools;
+
+// SeeDeepl-doc.md 
+class SystranTranslator extends RestClient implements TranslateInterface, DictionaryInterface {
+   
+   static private array  $trans_route = array('method' => 'POST', 'route' => "?????????????");     
+   static private array $lookup__route = array('method' => 'GET', 'route' => "?????????????");     
+
+   static private string $from_key = "????"; 
+   static private string $to_key = "????";  
+
+   private $query = array();
+   private $headers = array();
+ 
+   public function __construct(protected \SimpleXMLElement $provider, string $abbrev) 
+   {
+       parent::__construct($provider, $abbrev); 
+
+      $this->headers = [ ((string) $provider->settings->credentials->header['name']) => (string) $provider->settings->credentials->header]; 
+   }
+
+   // If this is a translation request and there is no source language, the source langauge will be auto-detected.
+   protected function setLanguages(string $dest_lang, $source_lang="")
+   {
+      if ($source_lang !== "") 
+           $this->query[self::$from_key] = $source_lang; 
+
+      $this->query[self::$to_key] = $dest_lang; 
+   }
+
+   final public function getTranslationLanguages() : array
+   {
+       $contents = $this->request(self::$method, self::$languages_route, ['headers' => $this->headers]);
+
+       return json_decode($contents, true);
+   }
+
+   final public function getDictionaryLanguages() : array
+   {
+      
+   }
+
+
+/*
+   public function getSourceLanguages() : array
+   {
+       $this->query['type'] = 'source';
+
+       $contents = $this->request(self::$method, self::$languages_route, ['headers' => $this->headers, 'query' => $this->query]); 
+
+       return json_decode($contents, true);
+   } 
+
+   public function getTargetLanguages() : array
+   {
+       $this->query['type'] = 'target';
+
+       $contents = $this->request(self::$method, self::$languages_route, ['headers' => $this->headers, 'query' => $this->query]); 
+
+       return json_decode($contents, true);
+   } 
+*/
+
+   final public function translate(string $text, string $dest_lang, $source_lang="") : string 
+   {
+       $this->setLanguages($dest_lang, $source_lang); 
+
+       $this->query['text'] = urlencode($text);
+
+       $contents = $this->request(self::$method, self::$trans_route, ['headers' => $this->headers, 'query' => $this->query]); 
+
+       $obj = json_decode($contents);
+
+       return urldecode($obj->translations????); 
+   }
+}
