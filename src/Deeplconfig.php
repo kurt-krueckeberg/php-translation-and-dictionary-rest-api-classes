f4c4fe889f6c08d2841cd48d247bd6968703d930
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-21 14:29:06 -0400

    updates

--- src/Deeplconfig.php
+++ src/Deeplconfig.php
@@ -2,40 +2,7 @@
 declare(strict_types=1);
 namespace LanguageTools;
 
-class Config {
+class Deeplconfig extends Config {
 
   
-  public static function getConfg(ClassID $id)
-  {
-    switch($id) {
-    case ClassID::LEIPZIG:
-     return new
-      break;
-
-    case ClassID::PONS:
-     return new
-      break;
-
-    case ClassID::SYSTRANS:
-     return new
-      break;
-
-    case ClassID::AZURE:
-     return new
-      break;
-
-    case ClassID::IBM:
-     return new
-      break;
-
-    case ClassID::YANDEX:
-     return new
-      break;
-
-    case ClassID::DEEPL:
-     return new
-      break;
-
-   }
-  }
 }
