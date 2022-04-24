88a6162bc687675d055a989b4e77e88ee3a8e47b
Kurt Kruecleberg <kurt.krueckeberg@gmail.com>
2022-04-24 15:43:54 -0400

    updates

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace LanguageTools;
 
-class ConfigIterface {
+interface ConfigInterface {
 
   public function get_endpoint() : string;
   public function get_authorization() : array | null ;  
