bda73f54d7544e3179448d30f3bc4311b8bdd310
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-24 14:34:51 -0400

    Add files via upload

---
+++ AzureTranslator.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types=1);
+namespace LanguageTools;
+
+class AzureTranslator extends /*RestClient*/ TranslatorWithDictionary implements DictionaryInterface, TranslateInterface {
+
+   static private array  $lookup = array('method' => "POST", 'route' => "dictionary/lookup");
+   static private array  $trans = array('method' => "POST", 'route' => "translate");
+   static private array  $languages = array('method' => "GET", 'route' => "languages");
+   static private string $from = "from";
+   static private string $to = "to";
+
+   // rquired query parameter 
+   private $query = array('api-version' => "3.0");
+   private $headers = array();
+   private $json = array();
+
+   
+   // Azure Translator REST calls can also accept a GUID
+   static private function com_create_guid() 
+   {
+       return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+           mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+           mt_rand( 0, 0xffff ),
+           mt_rand( 0, 0x0fff ) | 0x4000,
+           mt_rand( 0, 0x3fff ) | 0x8000,
+           mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+   }
+   
+   public function __construct(AzureConfig $c)
+   {
+       parent::__construct($c->get_endpoint());
+
+       foreach($c->get_authorization() as $key => $value) 
+          
+            $this->headers[$key] = $value;
+   }     
+
+   // Called by base Translator::translate method 
+   final protected function add_input(string $text)
+   {
+       $this->json = [['Text' => $text]];       
+   }
+
+   // If this is a translation request and there is no source language, the source langauge will be auto-detected.
+   protected function setLanguages(string $dest_lang, $source_lang="")
+   {
+      if ($source_lang !== "") 
+           $this->query[self::$from] = $source_lang; 
+
+      $this->query[self::$to] = $dest_lang; 
+   }
+
+   public function getTranslationLanguages() : array
+   {
+      $this->query['scope'] = 'translation';
+
+      $contents = $this->request(self::$languages['method'], self::$languages['route'],  ['headers' => $this->headers, 'query' => $this->query]);
+             
+      $arr = json_decode($contents, true);
+    
+      return $arr["translation"]; 
+    } 
+
+   final public function getDictionaryLanguages() : array // todo: check the actual array to confirm it is what we want.
+   {
+      $this->query['scope'] = 'dictionary';
+
+      $contents = $this->request(self::$languages['method'], self::$languages['route'],  ['headers' => $this->headers, 'query' => $this->query]);
+             
+      $obj = json_decode($contents, true);
+    
+      return $obj;
+   } 
+
+   final public function translate(string $text, string $dest_lang, $source_lang="") : string 
+   {
+       $this->setLanguages($dest_lang, $source_lang);
+
+       $this->add_input($text);
+
+       $response = $this->request(self::$trans['method'], self::$trans['route'], ['headers' => $this->headers, 'query' => $this->query, 'json' => $this->json]); 
+
+       $obj = json_decode($contents);
+
+       return $obj[0]->translations[0]->text; 
+   }
+
+   // Azure Translator offers dictionary lookup service that returns a one-word definition.
+   final public function lookup(string $word, string $src_lang, string $dest_lang) : string 
+   {
+      // 1. Set the dictionary languages
+      $this->setLanguages($dest_lang, $src_lang); 
+       
+      $this->add_input($word);
+
+      // 3. Issue post request 
+      $contents = $this->request(self::$lookup['method'], self::$lookup['route'], ['headers' => $this->headers, 'query' => $this->query, 'json' => $this->json]);
+
+      $obj = json_decode($contents)[0]; 
+      
+      return $obj->translations[0]->displayTarget; 
+   }
+}
---
+++ RestClient.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+namespace LanguageTools;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+
+class RestClient {
+
+   protected Client $client;  
+
+   static string $xpath =  "/providers/provider[@abbrev='%s']"; 
+
+/* 
+Currently Enumerations can't  be used as array keys. So this array won't work:
+
+   static array $class_map = array(
+        ClassID::Leipzig  => ['class' => 'SentenceFetcher',    'config' => 'Leipzigconfig'  ],
+        ClassID::Pons     => ['class' => 'PonsDictionary',     'config' => 'Ponsconfig'     ],
+        ClassID::Systrans => ['class' => 'SystransTranslator', 'config' => 'Systranconfig'  ],
+        ClassID::Azure    => ['class' => 'AzureTranslator',    'config' => 'Azureconfig'    ],
+        ClassID::Ibm      => ['class' => 'IbmTranslator',      'config' => 'Ivmconfig'      ],
+        ClassID::Yandex   => ['class' => 'YandexTranslator',   'config' => 'Yandexconfig'   ],
+        ClassID::Deepl    => ['class' => 'Deeplranslator',     'config' => 'Deeplconfig'    ],
+  );
+
+And we therfore can't do:
+
+     $arr = self::$class_map[$id]; 
+     $refl = new \ReflectionClass($arr['class']); 
+    
+     return $refl->newInstance(new $arr['config']);
+*/
+
+   static public function createRestClient(ClassID $id) : mixed
+   {
+      $class_name =  $id->class_name();
+
+      $refl = new \ReflectionClass($class_name); 
+      $x = $id->config_name();
+      
+      $v = new $x;
+      
+      return $refl->newInstance(new $id->config_name());
+   }
+
+   protected function request(string $method, string $route, array $options) : string
+   {
+       $response = $this->client->request($method, $route, $options);
+
+       return $response->getBody()->getContents();
+   }
+ 
+   protected function __construct(string $endpoint)
+   {     
+       $this->client = new Client(['base_uri' => $endpoint]);
+   } 
+}
