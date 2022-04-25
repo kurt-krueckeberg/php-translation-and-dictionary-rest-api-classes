da9981253fe66a2e402a82c07196f65dfbc786c9
Kurt Kruecleberg <kurt.krueckeberg@gmail.com>
2022-04-25 09:35:19 -0400

    updates

--- src/ClassmapperInterface.php
+++ src/ClassmapperInterface.php
@@ -3,7 +3,8 @@ declare(strict_types=1);
 namespace LanguageTools;
 
 interface ClassmapperInterface {
-      public function class_name() : string;
+
+    public function class_name() : string;
 
     /*  
     public function config_name() : string;
