4d34e95d23046718e937268428aa2ef7fd1b49c6
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-26 08:47:06 -0400

    Update SystransConfig.php

--- src/SystransConfig.php
+++ src/SystransConfig.php
@@ -3,13 +3,7 @@ declare(strict_types=1);
 namespace LanguageTools;
 
 class SystransConfig {
- /*
-   These yet-to-be-defined readonly properties are assigned (and promoted to class member variables) on the constructor:
-
-      public readonly string $endpoint = 
-      prublic readonly array $header = 
-  */
+ /*  These yet-to-be-defined readonly properties are assigned (and promoted to class member variables) on the constructor   */
   public function __construct(public readonly string $endpoint = "", 
       public readonly array $header = array()) {}
 }
-
