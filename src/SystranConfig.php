51a1b27d2efe8b5ed7e77b52d1b3671d66624ae4
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-27 15:23:28 -0400

    updates

--- src/SystranConfig.php
+++ src/SystranConfig.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 namespace LanguageTools;
 
 class SystranConfig {
-
- /*  These yet-to-be-defined readonly properties are assigned (and promoted to class member variables) on the constructor   */
-
+ /*
+   Client ID: HJ4lFfM7gBmTbYJj0BNO5
+   Client Secret: mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w
+  */
   public function __construct(public readonly string $endpoint = "", 
-      public readonly array $header = array()) {}
+      public readonly array $header = array('HJ4lFfM7gBmTbYJj0BNO5' => 'mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w')) {}
 }
