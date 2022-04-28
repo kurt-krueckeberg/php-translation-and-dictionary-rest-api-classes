640fe8981dbdeb769b32ddf9729388bec4c19399
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-27 20:52:25 -0400

    updates

--- src/SystranTranslator.php
+++ src/SystranTranslator.php
@@ -14,7 +14,12 @@ class SystranTranslator extends RestClient implements TranslateInterface, Dictio
    static private string $to = "target";
 
    private array $headers = array();
-   private array $query = array();
+   private array $query = array(); 
+
+   /*
+    There can be more than one query 'option' set; ii.e., the 'options' query paramter can occur more than one. 
+    I guess that would mean $thi->query['option'] = ['aaa', 'bbb', ... ];
+    */
    
    public function __construct(AzureConfig $c = new SystranConfig)
    {
@@ -26,8 +31,6 @@ class SystranTranslator extends RestClient implements TranslateInterface, Dictio
    // Called by base Translator::translate method 
    final protected function add_text(string $text)
    {
-      // todo: Does we also need to do utf-8, 
-      // The dfault expected encoding per docs is utf-8. This would be done before the urlencod()--right?  
        $this->query[self::$input] = urlencode($text);  
    }
 
@@ -40,14 +43,16 @@ class SystranTranslator extends RestClient implements TranslateInterface, Dictio
       return $arr["translation"]; 
     } 
 
-   final public function getDictionaryLanguages() : array // todo: check the actual array to confirm it is what we want.
+   final public function getDictionaryLanguages() : array
    {
       $contents = $this->request(self::$languages['method'], self::$languages['route'],  ['headers' => $this->headers, 'query' => $this->query]);
              
       return json_decode($contents, true);    
    } 
 
-   // If there is no source language, the source langauge will be auto-detected.
+  // If there is no source language, the source langauge will be auto-detected.
+  // todo: 
+  // The default expected encoding is utf-8. IF it is not utf-8 the 'options' paramter must be used to supply the endocing.
    protected function setLanguages(string $dest_lang, $source_lang="")
    {
       if ($source_lang !== "") 
@@ -58,18 +63,63 @@ class SystranTranslator extends RestClient implements TranslateInterface, Dictio
 
    final public function translate(string $text, string $dest_lang, $source_lang="") : string 
    {
-       $this->setLanguages($dest_lang, $source_lang);
+       $this->setLanguages($dest_lang, $source_lang); // If the language is not utf-8, then the 'options' query parameter must be used to sppecify the endocing.
 
        $this->add_text($text);
 
        $contents = $this->request(self::$trans['method'], self::$trans['route'], ['headers' => $this->headers, 'query' => $this->query]); 
 
        $obj = json_decode($contents);
+/*
+This is the response TranslationResponse object, according to https://docs.systran.net/translateAPI/translation/#tag-Translation
+
+{
+  "error": {
+    "message": "Internal error",
+    "statusCode": 500,
+    "info": "object"
+  },
+  "requestId": "55b753d87f2d682a607b8be1",
+  "outputs": [
+    {
+      "error": "Internal error",
+      "info": {
+        "lid": {
+          "language": "en",
+          "confidence": 0.9260174036026001
+        },
+        "selected_routes": [
+          {
+            "routes": [
+              {
+                "profileId": "6b80a3f5-aff2-41ba-a5ce-21603738432e",
+                "queue": "6b80a3f5-aff2-41ba-a5ce-21603738432e",
+                "service": "Translate_en_fr",
+                "version": 2,
+                "selectors": "object"
+              }
+            ],
+            "stepName": "filter-import"
+          }
+        ],
+        "stats": "object"
+      },
+      "output": "le chien",   <--------- Translation
+      "backTranslation": "the dog",
+      "source": "the dog"
+    }
+  ]
+}
+
+*/
 
        return $obj?????//[0]->translations[0]->text; 
    }
 
-   // Azure Translator offers dictionary lookup service that returns a one-word definition.
+
+   /*
+    *   Documentation at https://docs.systran.net/translateAPI/dictionary/
+    */
    final public function lookup(string $word, string $src_lang, string $dest_lang) : string 
    {      
       $this->setLanguages($dest_lang, $src_lang); 
