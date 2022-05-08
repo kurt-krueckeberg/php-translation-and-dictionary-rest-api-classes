abefa9a6f23dd28586ff54725a7d63da1770e8b0
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-05-08 09:56:34 -0400

    updates

--- src/DictionaryInterface.php
+++ src/DictionaryInterface.php
@@ -8,7 +8,7 @@ interface DictionaryInterface {
 
      Microsoft Azure returns a one-word string.
     */
-   public function lookup(string $str, string $src_lang, string $dest_lang) : string |array|ResultsIterator; 
+   public function lookup(string $str, string $src_lang, string $dest_lang) : array|ResultsIterator; 
 
    public function getDictionaryLanguages() : array; // todo:: Is getSupportedDictionaries() a beeter method name?
 }
