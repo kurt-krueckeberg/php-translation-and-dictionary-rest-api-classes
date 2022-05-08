24f780a659a9097496033bad8cb843b14c23a769
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-05-08 09:57:12 -0400

    updates

--- src/TranslateInterface.php
+++ src/TranslateInterface.php
@@ -4,6 +4,6 @@ namespace LanguageTools;
 
 interface TranslateInterface {
    // todo: Should this return an \Iterator or \ArrayIterator or 'string | ResitsIterator'?
-   public function translate(string $str, string $dest_lang, string $src_lang="") : string 
+   public function translate(string $str, string $dest_lang, string $src_lang="") : string;
    public function getTranslationLanguages() : array;
 }
