8ee902c732c5e532b3d692d6991c6123aafe44b4
kurt <kurt.krueckeberg@gmail.com>
2022-06-09 09:46:14 -0400

    updates

--- src/HtmlBuilder.php
+++ src/HtmlBuilder.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace LanguageTools;
 
-class CollinsHmtlPage {
+class HtmlBuilder {
 <?xml version="1.0" standalone="yes"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 :<html>
