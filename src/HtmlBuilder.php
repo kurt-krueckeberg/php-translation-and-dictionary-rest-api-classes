6025df30c8f123f226261e42cfcc1c1873e3126a
kurt <kurt.krueckeberg@gmail.com>
2022-06-10 17:45:24 -0400

    updates

--- src/HtmlBuilder.php
+++ src/HtmlBuilder.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=);
 namespace LanguageTools;
+use LanguageTools\SystranDictResult;
 
 class HtmlBuilder {
 
@@ -28,9 +29,44 @@ EOS;
 
     private \DOMDocument $dom;
      
+    private function insert_defn(ResultsIter $iter)
+    {
+       $doc = new \DOMDocument("1.0", "UTF8");
+
+       $doc->loadHTML(HtmlBuilder::$defn_html);
+
+       $doc->getElementsByTagName("h1")->item(0)->appendChild($doc->createTextNode($iter->term)); 
+
+       foreach($iter as $defns)  {
+
+           echo "<span class='pos'>"  . $defns->pos . "</span>";    // addNode
+ 
+           echo "<ul>\n"; // addNode
+
+           foreach ($defns->definitions as $defn) {
+
+                echo "<li>" . $defn["definition"] . "</li>\n";
+
+                if (isset($defn["expressions"])) {
+                    
+                   echo "<ul>\n"; 
+
+                   foreach ($defn['expressions'] as $expression) 
+
+                           echo "<li>". $expression->source  . " - ". $expression->target . "</li>\n";
+
+                   echo "</ul>\n";
+                }  
+           } 
+
+       }
+    }
+
     public function __construct()
     { 
        $this->dom = new \DOMDocument("1.0", "UTF8");
        $this->format = true; 
     }
+
+
 }
