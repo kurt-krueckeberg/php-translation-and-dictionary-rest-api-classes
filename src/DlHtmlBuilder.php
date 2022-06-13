222474061e792be62615e222789864e1bcdf6980
kurt <kurt.krueckeberg@gmail.com>
2022-06-13 18:46:07 -0400

    updates

--- src/DlHtmlBuilder.php
+++ src/DlHtmlBuilder.php
@@ -12,7 +12,7 @@ static private string $html_start = <<<html_eos
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de">
     <head>
-        <title>TODO supply a title</title>
+        <title>German Vocab</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="screen.css"> 
