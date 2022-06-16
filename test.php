054397b72c1563023bb23d543a1d62455ef28a68
kurt <kurt.krueckeberg@gmail.com>
2022-06-16 12:58:22 -0400

    updates

--- test.php
+++ test.php
@@ -7,19 +7,25 @@ function test()
  static $query = "//span[@class='gramGrp pos']";
  static $qtext = "//span[@class='gramGrp pos']/text()";
 
- $dom = new \DOMDocument("1.0", 'utf-8');
-$noun_html =<<<EOS
+static $noun_html =<<<EOS
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de">
     <head>
-        <title>German Vocab</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/pons.css"> 
     </head>
     <body>
-   <div class="entry_container">
+ </body>
+</html>
+EOS;
+
+ $dom = new \DOMDocument("1.0", 'utf-8');
+
+ $dom->loadHTML($noun_html);
+
+ $xpath = new \DOMXpath($dom);
+
+$div = <<<DIV
+<div class="entry_container">
   <div class="entry lang_de" id="befund_1">
     <h1 class="hwd">
       <span class="inline">Befund</span>
@@ -32,28 +38,20 @@ $noun_html =<<<EOS
     </div><!-- End of DIV hom-->
   </div><!-- End of DIV entry lang_de-->
 </div>
-</body>
-</html>
-EOS;
+DIV;
 
- $dom->loadHTML($noun_html);
+     $frag = $dom->createDocmentFragment();
 
- $xpath = new \DOMXpath($dom);
+     $frag->appendXML($html);
 
-  // try the most common query first....
-  $nodeList = $xpath->query($query);
+     $child = $dom->getElementsByTagName('body')->item(0)->appendChild($frag); 
 
-  var_dump($nodeList->item(0)); 
-  
-  
- $xpath = new \DOMXpath($dom);
 
   // try the most common query first....
   $nodeList = $xpath->query($qtext);
-  
-  echo "\n------------------\n";
 
   var_dump($nodeList->item(0)); 
+  echo "\n------------------\n";
 }
 
 test();
