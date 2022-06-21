5a69ccd9db78912147a50256ac7c8bbe233b2bc3
kurt <kurt.krueckeberg@gmail.com>
2022-06-21 07:03:03 -0400

    updates

--- src/PonsNounFetcher.php
+++ src/PonsNounFetcher.php
@@ -7,53 +7,51 @@ use LanguageTools\PonsDictionary;
 class PonsNounFetcher  {
       
       private \DOMDocument $dom;
-      private PonsDiciontary $dict;
+      private PonsDictionary $dict;
      
       private \DOMNode $body;
 
       private \DOMXpath $xpath;
 
-   public function __construct(PonsDictioary $dict)
+   public function __construct(PonsDictionary $dict)
    {   
-static $noun_start =<<<EOS
+static $html =<<<EOS
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de">
-    <head>
-    </head>
-    <body>
+  <head>
+  </head>
+  <body>
+  </body>
+</html>        
 EOS;
 
       $this->dict = $dict;
 
-      $html = $noun_start;
-
-      $html .= '</body.</html>';
-
       $this->dom = new \DOMDocument("1.0", 'utf-8');
-     
+      
       $this->dom->loadHTML($html);
 
-      $this->body = $this->dom->getElemtnsByTagName('body')->item(0);
+      $this->body = $this->dom->getElementsByTagName('body')->item(0);
 
       $this->xpath = new \DOMXpath($this->dom);
    } 
 
    public function __invoke(string $word) : string | null
    {       
-      static $query = '//span[@class="???"/acronym[@class="title"]';
+      static $query = "//span[@class='genus']/acronym[@class='title']";
 
-      $iter = $this->dict($word, "de", "en");
+      $iter = $this->dict->search($word, "de", "en");
 
-       foreach ($iter as $r) {
+      foreach ($iter as $r) {
             
          //  $x = strip_Tags($rom->headword); // and we must remove the spearator
          // $x = trim($x, "" )
          
          //if ($x == $word && isset($rom->wordclass)) {
-         if (isset($r->wordclass) && $r->wordclass == "noun") {
+         if (isset($r->pos) && $r->pos == "noun") {
 
-                 $hwf = "<p>" . $rom->headword_full . "</p>";
+                 $hwf = "<p>" . $r->headword_full . "</p>";
                  break;
          } 
        }
@@ -62,11 +60,11 @@ EOS;
 
        $frag->appendXML("<p>$hwf</p>");
 
-       $node = $hits->body->appendChild($frag);
+       $node = $this->body->appendChild($frag);
 
        $r = $this->xpath->query($query);
 
-       $hits->body->removeChild($node);
+       $this->body->removeChild($node);
 
        if ($r !== false) 
           return $r->textContext;        
